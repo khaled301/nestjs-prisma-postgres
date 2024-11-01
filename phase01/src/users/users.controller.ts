c0ceb068ca7d9545659eac9a916cc998a7f1b6da
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-11-01 20:02:42 +0600

    add basic routes structure

--- phase01/src/users/users.controller.ts
+++ phase01/src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+type UserRoleType = 'INTERN' | 'ADMIN' | 'ENGINEER';
+
+// decorator | route order matters like water fall
+@Controller('users') // route /users
+export class UsersController {
+
+
+    @Get() // GET /users or /users?role=value
+    findAll(@Query('role') role: Partial<UserRoleType>, @Query('department') department: string): string[]{
+        return [`${role}`, `${department}`];
+    }
+
+    @Get('interns') // GET /users/interns
+    findInterns(): []{
+        return []
+    }
+
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
+
+    @Post() // POST /users
+    createUser(@Body() user: {}) {
+        return user
+    }
+
+    @Patch(':id') // GET /users/:id
+    async update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate }
+    }
+
+    @Delete(':id') // GET /users/interns
+    delete(@Param('id') id: string){
+        return { id }
+    }
+}
